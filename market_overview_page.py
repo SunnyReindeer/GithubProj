885dea1f737b325d8822ea2dfdf75766ae682d2b
Shibee <a120250369@gmail.com>
2025-10-27 11:28:01 +0800

    11

--- market_overview_page.py
+++ market_overview_page.py
@@ -243,8 +243,9 @@ def get_fear_greed_index():
             valid_values = [int(n) for n in numbers if 0 <= int(n) <= 100]
             
             if valid_values:
-                # Filter out common false positives
-                filtered_values = [v for v in valid_values if v not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100]]
+                # Filter out very low numbers that are likely not the Fear & Greed Index
+                filtered_values = [v for v in valid_values if v >= 20]  # Fear & Greed Index is usually 20+
+                
                 if filtered_values:
                     # Return the most common reasonable value
                     most_common = max(set(filtered_values), key=filtered_values.count)
